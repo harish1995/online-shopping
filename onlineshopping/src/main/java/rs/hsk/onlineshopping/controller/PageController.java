9db4fc55990290dc9e0e6f2c60ef51c42a91b1cc
Harish <hkush1995@gmail.com>
2018-03-13 01:52:55 +0530

    successfully loaded static Reources

---
+++ onlineshopping/src/main/java/rs/hsk/onlineshopping/controller/PageController.java
@@ -0,0 +1,19 @@
+package rs.hsk.onlineshopping.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class PageController {
+	
+	
+	@RequestMapping(value= {"/" , "/home", "index"})
+	public ModelAndView index() {
+		ModelAndView mv = new ModelAndView("page");
+		mv.addObject("greeting", "welcome to web MVC");
+		return mv;
+	}
+}
+
+
